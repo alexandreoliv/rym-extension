c2aa1ae05c18cfac586f9e9470075de2454a0757
alexandreoliv <alexandrebo@gmail.com>
2022-07-29 02:38:30 +0200

    add rating to albums that don't have an ID

--- content_script.js
+++ content_script.js
@@ -9,19 +9,33 @@ const rymExtension = () => {
 
 			const myAlbums = getMyAlbums();
 			const trimmedMyAlbums = trimMyAlbums(myAlbums);
-			const pageAlbums = getPageAlbums();
-			const trimmedPageAlbums = getTrimmedPageAlbums(pageAlbums);
-			const foundAlbumsOnPage = findAlbumsOnPage(
-				trimmedPageAlbums,
+			const pageAlbumIds = getPageAlbumIds();
+			const trimmedPageAlbumIds = getTrimmedPageAlbumIds(pageAlbumIds);
+			const pageAlbumTitles = getPageAlbumTitles();
+
+			const foundAlbumsOnPageById = findAlbumsOnPageById(
+				trimmedPageAlbumIds,
 				trimmedMyAlbums
 			);
-			changeAlbumsOnPage(foundAlbumsOnPage, trimmedMyAlbums);
+
+			const foundAlbumsOnPageByTitle = findAlbumsOnPageByTitle(
+				pageAlbumTitles,
+				trimmedMyAlbums
+			);
+
+			console.log("foundAlbumsOnPageByTitle", foundAlbumsOnPageByTitle);
+
+			changeAlbumsOnPage(
+				foundAlbumsOnPageById,
+				foundAlbumsOnPageByTitle,
+				trimmedMyAlbums
+			);
 		});
 	}
 };
 
-const trimMyAlbums = (file) => {
-	return file.map((e) => ({
+const trimMyAlbums = (myAlbums) => {
+	return myAlbums.map((e) => ({
 		id: e["RYM Album"],
 		artist: e["First Name"] + " " + e["Last Name"],
 		artistLocalized:
@@ -31,7 +45,7 @@ const trimMyAlbums = (file) => {
 	}));
 };
 
-const getPageAlbums = () => {
+const getPageAlbumIds = () => {
 	const albums = document.getElementsByClassName("album");
 	const listAlbums = document.getElementsByClassName("list_album");
 	const albumIds = Array.prototype.map.call(albums, (a) => a.title);
@@ -40,21 +54,44 @@ const getPageAlbums = () => {
 	return allIds;
 };
 
-const getTrimmedPageAlbums = (file) => {
-	return file.map((a) => Number(a.slice(6, -1)));
+const getTrimmedPageAlbumIds = (pageAlbumIds) => {
+	return pageAlbumIds.map((a) => Number(a.slice(6, -1)));
 };
 
-const findAlbumsOnPage = (trimmedPageAlbums, trimmedMyAlbums) => {
+const getPageAlbumTitles = () => {
+	// TODO there might be homonyms
+	const albums = document.getElementsByClassName("ui_name_locale_original");
+	const albumTitles = Array.prototype.map.call(albums, (a) => a.innerText);
+	const release = document.getElementsByClassName("release");
+	const releaseTitles = Array.prototype.map.call(release, (a) => a.innerText);
+	const allTitles = albumTitles.concat(releaseTitles);
+	return allTitles;
+};
+
+const findAlbumsOnPageById = (trimmedPageAlbums, trimmedMyAlbums) => {
 	return trimmedPageAlbums.filter(
 		(p) =>
 			p === trimmedMyAlbums.filter((m) => m.id === p).map((m) => m.id)[0]
 	);
 };
 
-const changeAlbumsOnPage = (foundAlbumsOnPage, trimmedMyAlbums) => {
-	foundAlbumsOnPage.map((f) => {
+const findAlbumsOnPageByTitle = (trimmedPageAlbums, trimmedMyAlbums) => {
+	return trimmedPageAlbums.filter(
+		(p) =>
+			p ===
+			trimmedMyAlbums.filter((m) => m.album === p).map((m) => m.album)[0]
+	);
+};
+
+const changeAlbumsOnPage = (
+	foundAlbumsOnPageById,
+	foundAlbumsOnPageByTitle,
+	trimmedMyAlbums
+) => {
+	foundAlbumsOnPageById.map((f) => {
 		const elements = document.querySelectorAll(`[title="[Album${f}]"]`);
 		elements.forEach((element) => {
+			// prevents writing it twice
 			if (!element.innerHTML.includes("span style")) {
 				element.innerHTML += `<span style="font-weight: bold"> ${
 					trimmedMyAlbums
@@ -63,6 +100,21 @@ const changeAlbumsOnPage = (foundAlbumsOnPage, trimmedMyAlbums) => {
 				}</span>`;
 			}
 		});
+	});
+
+	console.log("foundAlbumsOnPageByTitle", foundAlbumsOnPageByTitle);
+	foundAlbumsOnPageByTitle.map((f) => {
+		const element = Array.from(document.querySelectorAll("a")).filter(
+			(a) => a.innerText === `${f}`
+		)[0];
+		console.log("element", element);
+		console.log(
+			"trimmedMyAlbums.filter((t) => t.album === f)",
+			trimmedMyAlbums.filter((t) => t.album === f)
+		);
+		element.innerHTML += `<span style="font-weight: bold"> ${
+			trimmedMyAlbums.filter((t) => t.album === f).map((t) => t.rating)[0]
+		}</span>`;
 	});
 };
 
